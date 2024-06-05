976382a91159ddb483332dc43584eb43b1f37c53
Xavier More <zetareticulae@proton.me>
2024-06-04 19:44:08 -0700

    increase density by using a renderable png
    codify an object as single file component

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -19,13 +19,13 @@ export const GET: APIRoute = async () => {
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
       <div style="display: flex; flex-direction: column; justify-content: space-between; width: 100%; filter: drop-shadow()">
         <div style="display: flex; flex-direction: column; gap: 0.75rem;">  
-          <p style="font-size: 48px;">Brutal</p>
-          <p style="font-size: 38px;">A theme for Astro</p>
-          <p style="font-size: 38px;">Brought to you by Elian</p>
+          <p style="font-size: 48px;">Zeta Reticula</p>
+          <p style="font-size: 38px;">A DOM of DOMs Vulcan powered IPFS JAMstore for JAMSTACK</p>
+          <p style="font-size: 38px;">Brought to you by the creators of EinsteinDB.</p>
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+        <img src="https://zetareticula.com/images/zr-logo.png" style="height: 100px; width: 100px;"/>
         </div>
       </div>
     </div>
